17ac7dcd81e61b57ce18cfbfe66d8b997fa35389
のぶ <nobuhiko.kimoto@gmail.com>
2012-12-02 14:16:46 +0900

    pagination use config file

--- fuel/app/classes/controller/user.php
+++ fuel/app/classes/controller/user.php
@@ -37,29 +37,7 @@ class controller_user extends Controller_Base
         $config = array(
             'pagination_url' => 'user/' . $this->username,
             'total_items'    => $this->log_count,
-            'per_page'       => 20,
             'uri_segment'    => 3,
-            'num_links'      => 5,
-            'template' => array(
-                'wrapper_start'           => '<div class="pagination pagination-centered"><ul>',
-                'wrapper_end'             => '</ul></div>',
-                'page_start'              => '',
-                'page_end'                => '',
-                'previous_start'          => '<li>',
-                'previous_end'            => '</li>',
-                'previous_inactive_start' => '<li class="disabled">',
-                'previous_inactive_end'   => '</li>',
-                'previous_mark'           => '',
-                'next_start'              => '<li>',
-                'next_end'                => '</li>',
-                'next_inactive_start'     => '<li class="disabled">',
-                'next_inactive_end'       => '</li>',
-                'next_mark'               => '',
-                'active_start'            => '<li class="active">',
-                'active_end'              => '</li>',
-                'regular_start'           => '<li>',
-                'regular_end'             => '</li>'
-            )
         );
         Pagination::set_config($config);
 
---
+++ fuel/app/config/pagination.php
@@ -0,0 +1,24 @@
+<?php
+return array(
+    'per_page' => 20,
+    'num_links' => 5,
+    'template' => array(
+        'wrapper_start'           => '<div class="pagination pagination-centered"><ul>',
+        'wrapper_end'             => '</ul></div>',
+        'page_start'              => '',
+        'page_end'                => '',
+        'previous_start'          => '<li>',
+        'previous_end'            => '</li>',
+        'previous_inactive_start' => '<li class="disabled">',
+        'previous_inactive_end'   => '</li>',
+        'previous_mark'           => '',
+        'next_start'              => '<li>',
+        'next_end'                => '</li>',
+        'next_inactive_start'     => '<li class="disabled">',
+        'next_inactive_end'       => '</li>',
+        'next_mark'               => '',
+        'active_start'            => '<li class="active">',
+        'active_end'              => '</li>',
+        'regular_start'           => '<li>',
+        'regular_end'             => '</li>'
+    ));
